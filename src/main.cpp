47737230061d6cae497d274b19b6c5082c2e433b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-06 17:17:32 +0000

    Deploy libhet/print_ip to github.com/libhet/print_ip.git:gh-pages

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "is_stl_container.h"
+#include "print_ip.h"
+
+
+#include <cmath>
+
+int main() {
+    try
+    {
+        print_ip(char(-1));
+        print_ip(short(0));
+        print_ip(2130706433);
+        print_ip(long(8875824491850138409));
+        print_ip("156.18.123.11");
+        print_ip(std::list<int>{17, 81, 65, 42});
+        print_ip(std::vector<int>{117, 161, 24, 42});
+    }
+    catch(std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
